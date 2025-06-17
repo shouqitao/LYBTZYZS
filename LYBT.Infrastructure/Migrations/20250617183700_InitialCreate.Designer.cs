f0f59521fa7f70bfd0a73169bbd72405b8d045d3
TonyShou <shouqitao@hotmail.com>
2025-06-18 02:43:10 +0800

    update code

--- LYBT.Infrastructure/Migrations/20250617183700_InitialCreate.Designer.cs
+++ LYBT.Infrastructure/Migrations/20250617183700_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LYBT.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250617170346_InitialCreate")]
+    [Migration("20250617183700_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
