01c4e8d91244413862091f2a52c8a41d1fb1e291
TonyShou <shouqitao@hotmail.com>
2025-06-30 21:16:58 +0800

    实现 挂号 和 管理员界面绘制

--- LYBT.UI.WPF/Views/AdminView.xaml.cs
+++ LYBT.UI.WPF/Views/AdminView.xaml.cs
@@ -5,5 +5,41 @@ namespace LYBT.UI.WPF.Views {
         public AdminView() {
             InitializeComponent();
         }
+
+        private void AddUser_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void EditUser_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void ToggleUserStatus_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void AddHerb_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void EditHerb_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void ToggleHerbStatus_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void AddPrescription_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void EditPrescription_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void TogglePrescriptionStatus_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
     }
 }
--- LYBT.UI.WPF/Views/RegistrationStaffView.xaml.cs
+++ LYBT.UI.WPF/Views/RegistrationStaffView.xaml.cs
@@ -5,5 +5,33 @@ namespace LYBT.UI.WPF.Views {
         public RegistrationStaffView() {
             InitializeComponent();
         }
+
+        private void btnReadCard_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void btnSearch_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void dgPatients_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void btnNewPatient_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void btnRegister_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void btnClear_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void btnCancel_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
     }
 }
