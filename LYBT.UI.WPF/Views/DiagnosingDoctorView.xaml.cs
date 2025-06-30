4fcf9ca98c7722d6f120ea246aa6930129347c60
TonyShou <shouqitao@hotmail.com>
2025-06-30 21:33:45 +0800

    完成看诊界面。

--- LYBT.UI.WPF/Views/DiagnosingDoctorView.xaml.cs
+++ LYBT.UI.WPF/Views/DiagnosingDoctorView.xaml.cs
@@ -5,5 +5,41 @@ namespace LYBT.UI.WPF.Views {
         public DiagnosingDoctorView() {
             InitializeComponent();
         }
+
+        private void PatientListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void SelectPatient_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void EmergencyPatient_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void DiagnosisHistoryListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void PrescriptionHistoryListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void AuxiliaryTreatmentHistoryListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void SuspendPatient_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void HoldPatient_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+
+        private void CompleteConsultation_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
     }
 }
