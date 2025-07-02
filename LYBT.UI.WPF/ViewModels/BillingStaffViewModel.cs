e77b8ed3baafecce34a6071b4b12dc91e3d9bec7
TonyShou <shouqitao@hotmail.com>
2025-07-03 06:08:45 +0800

    feat(wpf): add billing api integration

--- LYBT.UI.WPF/ViewModels/BillingStaffViewModel.cs
+++ LYBT.UI.WPF/ViewModels/BillingStaffViewModel.cs
@@ -1,6 +1,50 @@
+using LYBT.Module.Billing.Dtos;
+using LYBT.UI.WPF.Services;
+using Prism.Commands;
 using Prism.Mvvm;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace LYBT.UI.WPF.ViewModels {
     public class BillingStaffViewModel : BindableBase {
+        public ObservableCollection<BillingDto> Bills { get; } = new();
+
+        private BillingDto? _selectedBill;
+        public BillingDto? SelectedBill {
+            get => _selectedBill;
+            set => SetProperty(ref _selectedBill, value);
+        }
+
+        public DelegateCommand RefreshCommand { get; }
+        public DelegateCommand MarkPaidCommand { get; }
+
+        private readonly IBillingService _billingService;
+
+        public BillingStaffViewModel(IBillingService billingService) {
+            _billingService = billingService;
+            RefreshCommand = new DelegateCommand(async () => await LoadBills());
+            MarkPaidCommand = new DelegateCommand(async () => await MarkPaid(), () => SelectedBill != null)
+                .ObservesProperty(() => SelectedBill);
+            _ = LoadBills();
+        }
+
+        private async Task LoadBills() {
+            var list = await _billingService.GetAllAsync();
+            Bills.Clear();
+            foreach (var b in list)
+                Bills.Add(b);
+        }
+
+        private async Task MarkPaid() {
+            if (SelectedBill == null)
+                return;
+            bool ok = await _billingService.MarkAsPaidAsync(SelectedBill.Id);
+            if (ok)
+                await LoadBills();
+            else
+                MessageBox.Show("操作失败", "提示", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }
