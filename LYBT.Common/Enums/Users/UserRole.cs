a51beb81e107a1329fc82432aa3eca7a5ebc925f
TonyShou <shouqitao@hotmail.com>
2025-07-03 23:51:59 +0800

    update code

--- LYBT.Common/Enums/Users/UserRole.cs
+++ LYBT.Common/Enums/Users/UserRole.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace LYBT.Common.Enums.Users {
 
     /// <summary>
@@ -6,21 +8,27 @@ namespace LYBT.Common.Enums.Users {
     public enum UserRole {
 
         /// <summary>系统管理员</summary>
+        [Description("系统管理员")]
         Admin = 0,
 
         /// <summary>主治医生</summary>
+        [Description("主治医生")]
         DiagnosingDoctor = 1,
 
         /// <summary>理疗师</summary>
+        [Description("理疗师")]
         TreatmentDoctor = 2,
 
         /// <summary>药剂师</summary>
+        [Description("药剂师")]
         PharmacyStaff = 3,
 
         /// <summary>挂号人员</summary>
+        [Description("挂号人员")]
         RegistrationStaff = 4,
 
         /// <summary>收费人员</summary>
+        [Description("收费人员")]
         BillingStaff = 5
     }
 }
