00260a3699acdcf3057c712cbde406894d5ef13b
TonyShou <shouqitao@hotmail.com>
2025-07-03 10:38:01 +0800

    Add WPF record service

---
+++ LYBT.UI.WPF/Services/IRecordService.cs
@@ -0,0 +1,18 @@
+using LYBT.Module.Records.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LYBT.UI.WPF.Services {
+    public interface IRecordService {
+        Task<IList<RecordDto>> GetListAsync();
+        Task<IList<RecordDto>> GetByPatientIdAsync(Guid patientId);
+        Task<RecordDetailDto?> GetByIdAsync(Guid id);
+        Task<bool> AddAsync(RecordCreateDto dto);
+        Task<bool> UpdateAsync(RecordEditDto dto);
+        Task<bool> DeleteAsync(Guid id);
+        Task<bool> MarkAsSharedAsync(Guid id, List<string> doctorIds);
+        Task<bool> RevokeSharingAsync(Guid id);
+        Task<IList<RecordDto>> GetSharedRecordsAsync(Guid doctorId);
+    }
+}
