5182f1d0880b04a1e9f6f6585c2ce7caaca7e2b8
TonyShou <shouqitao@hotmail.com>
2025-07-04 22:22:17 +0800

    feat(ui): add doctor info view

---
+++ LYBT.UI.WPF/Views/Main/DoctorInfoView.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows.Controls;
+
+namespace LYBT.UI.WPF.Views.Main {
+    public partial class DoctorInfoView : UserControl {
+        public DoctorInfoView() {
+            InitializeComponent();
+        }
+    }
+}
