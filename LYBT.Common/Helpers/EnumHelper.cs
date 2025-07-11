b9774a9b28130fc90a5d84aa0ce09e50163bb009
TonyShou <shouqitao@hotmail.com>
2025-07-12 07:36:22 +0800

    Implement helpers and use EnumHelper in WPF

---
+++ LYBT.Common/Helpers/EnumHelper.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.ObjectModel;
+using LYBT.Common.Extensions;
+using LYBT.Common.Models;
+
+namespace LYBT.Common.Helpers {
+    /// <summary>
+    /// Helper methods for working with enums.
+    /// </summary>
+    public static class EnumHelper {
+        /// <summary>
+        /// Build an observable collection suitable for binding to a ComboBox.
+        /// Each item contains the enum value and its description text.
+        /// </summary>
+        public static ObservableCollection<EnumItem<T>> BuildComboBoxSource<T>() where T : Enum {
+            var list = new ObservableCollection<EnumItem<T>>();
+            foreach (T value in Enum.GetValues(typeof(T))) {
+                list.Add(new EnumItem<T>(value, value.GetDescription()));
+            }
+            return list;
+        }
+    }
+}
