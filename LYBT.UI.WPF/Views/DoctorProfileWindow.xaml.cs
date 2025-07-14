cb68e468aa6f77ad29e94dd3d15cd93a0c53def4
TonyShou <shouqitao@hotmail.com>
2025-07-14 22:14:38 +0800

    feat(ui): open doctor profile view when creating

---
+++ LYBT.UI.WPF/Views/DoctorProfileWindow.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows;
+
+namespace LYBT.UI.WPF.Views {
+    public partial class DoctorProfileWindow : Window {
+        public DoctorProfileWindow() {
+            InitializeComponent();
+        }
+    }
+}
