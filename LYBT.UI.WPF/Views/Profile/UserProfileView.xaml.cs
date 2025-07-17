5f2e2216b4eeae9b44f8ef7e07483d841017f03b
TonyShou <shouqitao@hotmail.com>
2025-07-17 16:12:35 +0800

    Remove unused user views

--- LYBT.UI.WPF/Views/Profile/UserProfileView.xaml.cs
+++
@@ -1,9 +0,0 @@
-using System.Windows.Controls;
-
-namespace LYBT.UI.WPF.Views.Profile {
-    public partial class UserProfileView : UserControl {
-        public UserProfileView() {
-            InitializeComponent();
-        }
-    }
-}
