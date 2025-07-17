4b8dec22c21a1e32097bef3aad786699eff72b9c
TonyShou <shouqitao@hotmail.com>
2025-07-17 16:41:09 +0800

    refactor: unify profile views

--- LYBT.UI.WPF/Views/DoctorProfileWindow.xaml.cs
+++
@@ -1,9 +0,0 @@
-using System.Windows;
-
-namespace LYBT.UI.WPF.Views {
-    public partial class DoctorProfileWindow : Window {
-        public DoctorProfileWindow() {
-            InitializeComponent();
-        }
-    }
-}
