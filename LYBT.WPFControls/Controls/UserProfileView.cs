47e98120996fe23e6122e1789ea6b9f92e80890b
TonyShou <shouqitao@gmail.com>
2025-07-19 16:14:52 +0800

    fix: mark user views as content controls

--- LYBT.WPFControls/Controls/UserProfileView.cs
+++ LYBT.WPFControls/Controls/UserProfileView.cs
@@ -1,3 +1,9 @@
+using System.Windows.Markup;
+
 namespace LYBT.WPFControls {
+    /// <summary>
+    /// Profile editing view for user accounts.
+    /// </summary>
+    [ContentProperty(nameof(ProfileContent))]
     public class UserProfileView : CommonProfileView { }
 }
