cb61af8203be4ec85d3efef49086851f2006eae7
TonyShou <shouqitao@gmail.com>
2025-07-26 15:52:03 +0800

    Introduce PatientsDbContext and module

---
+++ LYBT.Module.Patients/PatientsModule.cs
@@ -0,0 +1,23 @@
+using LYBT.Module.Patients.Interfaces;
+using LYBT.Module.Patients.Repositories;
+using LYBT.Module.Patients.Services;
+using LYBT.Module.Patients.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LYBT.Module.Patients;
+
+/// <summary>
+/// Dependency registration for patients module.
+/// </summary>
+public static class PatientsModule {
+    /// <summary>
+    /// Register patients module services and DbContext.
+    /// </summary>
+    public static IServiceCollection AddPatientsModule(this IServiceCollection services, string connectionString) {
+        services.AddDbContext<PatientsDbContext>(opts => opts.UseSqlServer(connectionString));
+        services.AddScoped<IPatientRepository, PatientRepository>();
+        services.AddScoped<IPatientService, PatientService>();
+        return services;
+    }
+}
