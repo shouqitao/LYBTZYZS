63c63a78beb098eda9b4830287b22ee5d53323ee
shouqitao <shouqitao@126.com>
2026-07-03 14:02:25 +0800

    refactor: 清理Domain+Infrastructure层死代码(10个文件)
    SharedKernel:
    - 删除ValueObject.cs(零继承者)
    - 删除Outbox/OutboxMessage.cs+IOutboxService.cs(零实现)
    - 删除DTOs/UserBasicDto.cs(被Shared.Models版本替代)
    - 删除Contracts/IUserCrossModuleService.cs(被Infrastructure版本替代)
    - 删除Contracts/IPatientQueryService.cs(未实现接口)
    
    Entities:
    - 删除ApplicationRole.cs(未使用的IdentityRole子类)
    
    Infrastructure:
    - 删除IDefaultPasswordService+DefaultPasswordService(未注册DI)
    - 删除HttpContextCorrelationIdProvider(未注册DI)

--- src/Server/Core/LYBT.SharedKernel/Primitives/ValueObject.cs
+++
@@ -1,9 +0,0 @@
-namespace LYBT.SharedKernel.Primitives;
-
-/// <summary>
-/// 值对象基类。不可变，无身份标识，通过属性值相等性比较。
-/// record类型自动提供基于属性的相等性比较，无需额外实现。
-/// </summary>
-public abstract record ValueObject;
-
-
