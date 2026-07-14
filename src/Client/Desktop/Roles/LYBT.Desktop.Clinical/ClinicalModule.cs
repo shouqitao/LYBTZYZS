4465512129cb6b5f8fdeb2fc47322dc4a7264114
shouqitao <shouqitao@126.com>
2026-07-15 06:13:47 +0800

    refactor(clinical): remove dead PendingQueueView navigation registration

--- src/Client/Desktop/Roles/LYBT.Desktop.Clinical/ClinicalModule.cs
+++ src/Client/Desktop/Roles/LYBT.Desktop.Clinical/ClinicalModule.cs
@@ -35,9 +35,6 @@ namespace LYBT.Desktop.Clinical
             containerRegistry.RegisterForNavigation<Views.FormulaManagementView>();
             containerRegistry.RegisterForNavigation<Views.PatientManagementView>();
             containerRegistry.RegisterForNavigation<Views.MedicalCaseManagementView>();
-            
-            // 注册PendingQueueView
-            containerRegistry.RegisterForNavigation<Views.PendingQueueView>();
         }
     }
 }
